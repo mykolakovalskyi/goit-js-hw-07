3b09961a417878f90f420483f867b71d604d66b3
Mykola Kovalskyi <112631611+mykolakovalskyi@users.noreply.github.com>
2023-02-27 01:10:43 +0100

    SimpleLightbox added

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -6,17 +6,15 @@ const gallery = document.querySelector(".gallery");
 const galleryItem = galleryItems
   .map(
     (item) =>
-      `<div class="gallery__item">
-  <a class="gallery__link" href="${item.original}">
-    <img
-      class="gallery__image"
-      src="${item.preview}"
-      data-source="${item.original}"
-      alt="${item.description}"
-    />
-  </a>
-</div>`
+      `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`
   )
   .join("");
 
 gallery.insertAdjacentHTML("beforeend", galleryItem);
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
